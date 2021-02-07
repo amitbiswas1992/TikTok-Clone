d2aec35e59cf7d5d194b55bf7ba8b1fa0fcc84b5
Amit Biswas <biswas1992@hotmail.com>
2021-02-07 12:01:56 -0500

    Initiate feedback generator for haptic manager

--- TikTok-Clone/Managers/HapticManager.swift
+++ TikTok-Clone/Managers/HapticManager.swift
@@ -13,7 +13,25 @@ final class HapticManager {
     
     private init () {}
     
+    public func vibrateForSelection() {
+        
+        DispatchQueue.main.async {
+            
+            let generator = UISelectionFeedbackGenerator()
+            generator.prepare()
+            generator.selectionChanged()
+        }
+    }
     
+    public func vibrate(for type: UINotificationFeedbackGenerator.FeedbackType) {
+        
+        DispatchQueue.main.async {
+            
+            let generator = UINotificationFeedbackGenerator()
+            generator.prepare()
+            generator.notificationOccurred(type)
+        }
+    }
     
     
     
