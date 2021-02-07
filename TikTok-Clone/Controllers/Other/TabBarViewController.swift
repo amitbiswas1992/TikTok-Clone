f4da3060458128ed1509df5c907e939817c71cab
Amit Biswas <biswas1992@hotmail.com>
2021-02-06 23:21:33 -0500

    Setup tabbar, navbar for each controllers and title for each VC's

--- TikTok-Clone/Controllers/Other/TabBarViewController.swift
+++ TikTok-Clone/Controllers/Other/TabBarViewController.swift
@@ -11,19 +11,36 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupController()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupController() {
+        
+        // Created VC's Objects/ instance
+        let home            = HomeViewController()
+        let profile         = ProfileViewController()
+        let camera          = CameraViewController()
+        let notification    = NotificationViewController()
+        let explore         = ExploreViewController()
+        
+        // set Each VC's Title
+        home.title          = "Home"
+        profile.title       = "Profile"
+        camera.title        = "Camera"
+        notification.title  = "Notification"
+        explore.title       = "Explore"
+        
+        // Setup VC on Navigation Controller
+        let homeNav         = UINavigationController(rootViewController: home)
+        let profileNav      = UINavigationController(rootViewController: profile)
+        let notificationNav = UINavigationController(rootViewController: notification)
+        let exploreNav      = UINavigationController(rootViewController: explore)
+        
+        // Indexing navbar ccontrollers inside setVC Array linear way
+        setViewControllers([homeNav,profileNav,camera, notificationNav,exploreNav], animated: false)
+        
+        
     }
-    */
 
 }
